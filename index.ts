9e0f3dd2e020daeb67e30d49e0cf3363f30008ef
Vladislav Botvin <darkvlados@gmail.com>
2026-03-21 00:52:49 +0300

    predicate type narrowing progress

--- index.ts
+++ index.ts
@@ -32,23 +32,27 @@ export function multimethod<
   const K3,
   const K4,
   const K5,
-  const RestKs extends readonly any[],
+  const K6,
+  const K7,
+  const K8,
+  const K9,
+  const K10,
 >(
   predicateFn: (arg: A) => P,
   defaultFn: (arg: P) => R,
   c1: readonly [
-    ExhaustiveCheck<P, K1 | K2 | K3 | K4 | K5 | RestKs[number]>,
+    ExhaustiveCheck<P, K1 | K2 | K3 | K4 | K5 | K6 | K7 | K8 | K9 | K10>,
     (arg: K1) => R,
   ],
   c2: readonly [K2, (arg: K2) => R],
   c3: readonly [K3, (arg: K3) => R],
   c4: readonly [K4, (arg: K4) => R],
   c5: readonly [K5, (arg: K5) => R],
-  ...rest: {
-    [I in keyof RestKs]: RestKs[I] extends readonly [infer K, any]
-      ? readonly [K, (arg: K) => R]
-      : never;
-  }
+  c6: readonly [K6, (arg: K6) => R],
+  c7: readonly [K7, (arg: K7) => R],
+  c8: readonly [K8, (arg: K8) => R],
+  c9: readonly [K9, (arg: K9) => R],
+  c10: readonly [K10, (arg: K10) => R],
 ): (arg: A) => R;
 
 export function multimethod<
@@ -59,22 +63,26 @@ export function multimethod<
   const K2,
   const K3,
   const K4,
-  const RestKs extends readonly any[],
+  const K5,
+  const K6,
+  const K7,
+  const K8,
+  const K9,
 >(
   predicateFn: (arg: A) => P,
   defaultFn: (arg: P) => R,
   c1: readonly [
-    ExhaustiveCheck<P, K1 | K2 | K3 | K4 | RestKs[number]>,
+    ExhaustiveCheck<P, K1 | K2 | K3 | K4 | K5 | K6 | K7 | K8 | K9>,
     (arg: K1) => R,
   ],
   c2: readonly [K2, (arg: K2) => R],
   c3: readonly [K3, (arg: K3) => R],
   c4: readonly [K4, (arg: K4) => R],
-  ...rest: {
-    [I in keyof RestKs]: RestKs[I] extends readonly [infer K, any]
-      ? readonly [K, (arg: K) => R]
-      : never;
-  }
+  c5: readonly [K5, (arg: K5) => R],
+  c6: readonly [K6, (arg: K6) => R],
+  c7: readonly [K7, (arg: K7) => R],
+  c8: readonly [K8, (arg: K8) => R],
+  c9: readonly [K9, (arg: K9) => R],
 ): (arg: A) => R;
 
 export function multimethod<
@@ -84,21 +92,25 @@ export function multimethod<
   const K1,
   const K2,
   const K3,
-  const RestKs extends readonly any[],
+  const K4,
+  const K5,
+  const K6,
+  const K7,
+  const K8,
 >(
   predicateFn: (arg: A) => P,
   defaultFn: (arg: P) => R,
   c1: readonly [
-    ExhaustiveCheck<P, K1 | K2 | K3 | RestKs[number]>,
+    ExhaustiveCheck<P, K1 | K2 | K3 | K4 | K5 | K6 | K7 | K8>,
     (arg: K1) => R,
   ],
   c2: readonly [K2, (arg: K2) => R],
   c3: readonly [K3, (arg: K3) => R],
-  ...rest: {
-    [I in keyof RestKs]: RestKs[I] extends readonly [infer K, any]
-      ? readonly [K, (arg: K) => R]
-      : never;
-  }
+  c4: readonly [K4, (arg: K4) => R],
+  c5: readonly [K5, (arg: K5) => R],
+  c6: readonly [K6, (arg: K6) => R],
+  c7: readonly [K7, (arg: K7) => R],
+  c8: readonly [K8, (arg: K8) => R],
 ): (arg: A) => R;
 
 export function multimethod<
@@ -107,17 +119,24 @@ export function multimethod<
   R,
   const K1,
   const K2,
-  const RestKs extends readonly any[],
+  const K3,
+  const K4,
+  const K5,
+  const K6,
+  const K7,
 >(
   predicateFn: (arg: A) => P,
   defaultFn: (arg: P) => R,
-  c1: readonly [ExhaustiveCheck<P, K1 | K2 | RestKs[number]>, (arg: K1) => R],
+  c1: readonly [
+    ExhaustiveCheck<P, K1 | K2 | K3 | K4 | K5 | K6 | K7>,
+    (arg: K1) => R,
+  ],
   c2: readonly [K2, (arg: K2) => R],
-  ...rest: {
-    [I in keyof RestKs]: RestKs[I] extends readonly [infer K, any]
-      ? readonly [K, (arg: K) => R]
-      : never;
-  }
+  c3: readonly [K3, (arg: K3) => R],
+  c4: readonly [K4, (arg: K4) => R],
+  c5: readonly [K5, (arg: K5) => R],
+  c6: readonly [K6, (arg: K6) => R],
+  c7: readonly [K7, (arg: K7) => R],
 ): (arg: A) => R;
 
 export function multimethod<
@@ -125,16 +144,72 @@ export function multimethod<
   P,
   R,
   const K1,
-  const RestKs extends readonly any[],
+  const K2,
+  const K3,
+  const K4,
+  const K5,
+  const K6,
 >(
   predicateFn: (arg: A) => P,
   defaultFn: (arg: P) => R,
-  c1: readonly [ExhaustiveCheck<P, K1 | RestKs[number]>, (arg: K1) => R],
-  ...rest: {
-    [I in keyof RestKs]: RestKs[I] extends readonly [infer K, any]
-      ? readonly [K, (arg: K) => R]
-      : never;
-  }
+  c1: readonly [
+    ExhaustiveCheck<P, K1 | K2 | K3 | K4 | K5 | K6>,
+    (arg: K1) => R,
+  ],
+  c2: readonly [K2, (arg: K2) => R],
+  c3: readonly [K3, (arg: K3) => R],
+  c4: readonly [K4, (arg: K4) => R],
+  c5: readonly [K5, (arg: K5) => R],
+  c6: readonly [K6, (arg: K6) => R],
+): (arg: A) => R;
+
+export function multimethod<
+  A,
+  P,
+  R,
+  const K1,
+  const K2,
+  const K3,
+  const K4,
+  const K5,
+>(
+  predicateFn: (arg: A) => P,
+  defaultFn: (arg: P) => R,
+  c1: readonly [ExhaustiveCheck<P, K1 | K2 | K3 | K4 | K5>, (arg: K1) => R],
+  c2: readonly [K2, (arg: K2) => R],
+  c3: readonly [K3, (arg: K3) => R],
+  c4: readonly [K4, (arg: K4) => R],
+  c5: readonly [K5, (arg: K5) => R],
+): (arg: A) => R;
+
+export function multimethod<A, P, R, const K1, const K2, const K3, const K4>(
+  predicateFn: (arg: A) => P,
+  defaultFn: (arg: P) => R,
+  c1: readonly [ExhaustiveCheck<P, K1 | K2 | K3 | K4>, (arg: K1) => R],
+  c2: readonly [K2, (arg: K2) => R],
+  c3: readonly [K3, (arg: K3) => R],
+  c4: readonly [K4, (arg: K4) => R],
+): (arg: A) => R;
+
+export function multimethod<A, P, R, const K1, const K2, const K3>(
+  predicateFn: (arg: A) => P,
+  defaultFn: (arg: P) => R,
+  c1: readonly [ExhaustiveCheck<P, K1 | K2 | K3>, (arg: K1) => R],
+  c2: readonly [K2, (arg: K2) => R],
+  c3: readonly [K3, (arg: K3) => R],
+): (arg: A) => R;
+
+export function multimethod<A, P, R, const K1, const K2>(
+  predicateFn: (arg: A) => P,
+  defaultFn: (arg: P) => R,
+  c1: readonly [ExhaustiveCheck<P, K1 | K2>, (arg: K1) => R],
+  c2: readonly [K2, (arg: K2) => R],
+): (arg: A) => R;
+
+export function multimethod<A, P, R, const K1>(
+  predicateFn: (arg: A) => P,
+  defaultFn: (arg: P) => R,
+  c1: readonly [ExhaustiveCheck<P, K1>, (arg: K1) => R],
 ): (arg: A) => R;
 
 export function multimethod<A, P, R>(
